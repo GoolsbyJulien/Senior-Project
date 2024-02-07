04e9c2a803fe972bf2089644501b6304dcdf7df0
AustinLBarnes <alb89490@ucmo.edu>
2024-02-07 13:47:25 -0600

    New Project work

--- src/com/jra/app/UI/views/TopMenu.java
+++ src/com/jra/app/UI/views/TopMenu.java
@@ -5,6 +5,7 @@ import com.jra.app.Project;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileWriter;
@@ -18,7 +19,7 @@ public class TopMenu extends JMenuBar {
     private JMenuItem fileNew = new JMenuItem(new NewProjectAction());
     private JMenuItem fileOpen = new JMenuItem(new LoadMapAction());
     private JMenuItem fileSave = new JMenuItem(new SaveMapAction());
-    private JMenuItem fileSaveImage = new JMenuItem("Save current view as Image");
+    private JMenuItem fileSaveImage = new JMenuItem(new SaveImageAction());
     private JMenu menuView = new JMenu("View");
     private JMenu viewMapView = new JMenu("Map View");
     public Project currentProject;
@@ -54,6 +55,40 @@ public class TopMenu extends JMenuBar {
     }
 
     public void newProject() {
+        EventQueue.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                //Create new project window
+                JFrame frame = new JFrame("Create new project");
+                frame.setLayout(new GridLayout());
+                frame.setSize(500,500);
+
+                //Components
+                Label titleLabel = new Label("Project Title");
+                TextField titleField = new TextField();
+                Label descriptionLabel = new Label("Project Description");
+                TextArea descriptionArea = new TextArea();
+                Label label = new Label("Choose a base image from the options below:");
+                Button perlinButton = new Button("Perlin Noise");
+                Button imageButton = new Button("Image");
+
+                //Add components to frame
+                frame.add(titleLabel,0);
+                frame.add(titleField,0);
+                frame.add(descriptionLabel,1);
+                frame.add(descriptionArea,1);
+                frame.add(label,2);
+                frame.add(perlinButton,3);
+                frame.add(imageButton,3);
+
+                frame.setVisible(true);
+            }
+        });
+    }
+
+    public void loadNewPerlinScene(){
         Main.instance.world.generateMap();
     }
 
@@ -168,5 +203,15 @@ class NewProjectAction extends AbstractAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         new TopMenu().newProject();
+    }
+}
+
+class SaveImageAction extends AbstractAction{
+    public SaveImageAction() {
+        super("Save current view as Image");
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        new TopMenu().saveImage();
     }
 }
