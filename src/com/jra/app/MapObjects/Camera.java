32ce10fcf48534e7a33847946c58c064d7a343d4
Julien Goolsby <juliengoolsby@gmail.com>
2024-02-20 23:56:09 -0600

    Camera Drag

--- src/com/jra/app/MapObjects/Camera.java
+++ src/com/jra/app/MapObjects/Camera.java
@@ -4,6 +4,8 @@ import com.jra.api.core.MapObject;
 import com.jra.api.input.Keyboard;
 import com.jra.api.input.Mouse;
 import com.jra.api.render.MapRenderer;
+import com.jra.api.util.Vector;
+import com.jra.app.Main;
 
 import java.awt.*;
 
@@ -20,6 +22,8 @@ public class Camera extends MapObject {
     }
 
     private float camSpeed = 10;
+    boolean isMovingTowards = false;
+    Vector moveTowards = new Vector(0, 0);
 
     @Override
     public void tick() {
@@ -35,6 +39,24 @@ public class Camera extends MapObject {
         else if (Keyboard.A || Keyboard.LEFT)
             pos.x -= camSpeed;
 
+        int mouseX = (int) ((Mouse.mousePos.x + Main.instance.mapRenderer.cameraPosition.x * Main.instance.mapRenderer.cameraZoom) / Main.instance.mapRenderer.cameraZoom);
+        int mouseY = (int) ((Mouse.mousePos.y + Main.instance.mapRenderer.cameraPosition.y * Main.instance.mapRenderer.cameraZoom) / Main.instance.mapRenderer.cameraZoom);
+
+        if (Mouse.RIGHT_CLICK) {
+
+            if (!isMovingTowards) {
+                moveTowards.x = mouseX;
+                moveTowards.y = mouseY;
+                isMovingTowards = true;
+            } else {
+                int deltaX = mouseX - moveTowards.x;
+                int deltaY = mouseY - moveTowards.y;
+                pos.x -= deltaX;
+                pos.y -= deltaY;
+            }
+        } else {
+            isMovingTowards = false;
+        }
 
         window.cameraZoom = Mouse.wheel;
         window.cameraPosition = pos;
