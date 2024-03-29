588ad9bc354ef43a918c7d1b0a7f5ce298cf7298
AustinLBarnes <alb89490@ucmo.edu>
2024-03-29 18:28:20 -0500

    Format right panel

--- src/com/jra/app/UI/views/RightPanel.java
+++ src/com/jra/app/UI/views/RightPanel.java
@@ -12,7 +12,9 @@ import java.awt.event.ActionListener;
 public class RightPanel extends JPanel {
 
     SelectableObject currentObject = null;
+    public JPanel panel = new JPanel(new GridBagLayout());
     JTextField name = new JTextField();
+    JTextArea description = new JTextArea(5, 2);
 
 
     public  void update(SelectableObject object){
@@ -21,21 +23,56 @@ public class RightPanel extends JPanel {
     }
     public RightPanel() {
         PanelButton button = new PanelButton("Right Panel");
-
-
         setLayout(new BorderLayout());
+
         add(button, BorderLayout.NORTH);
         setLocation(0, 0);
         setPreferredSize(new Dimension(300, 800));
-        name.setFont(StyleGlobals.getFont(10));
+
+        //Grid bag constraints
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.ipadx = 15;
+        c.ipady = 10;
+
+        JLabel nameLabel = new JLabel("Name");
+        nameLabel.setForeground(Color.WHITE);
+        panel.add(nameLabel, c);
+
+        name.setFont(StyleGlobals.getFont(15));
         name.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 SelectableObject.currentObject.setLabel(name.getText());
             }
         });
-        add(name, BorderLayout.CENTER);
-        setBackground(StyleGlobals.BACKGROUND);
+        c.gridx = 1;
+        c.ipadx = 150;
+        c.ipady = 10;
+        panel.add(name, c);
 
+        JLabel descriptionLabel = new JLabel("Description");
+        descriptionLabel.setForeground(Color.WHITE);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.ipadx = 15;
+        c.ipady = 80;
+        panel.add(descriptionLabel, c);
+
+        //Description area
+        JScrollPane descriptionPane = new JScrollPane();
+        description.setFont(StyleGlobals.getFont(15));
+        description.setLineWrap(true);
+        description.setWrapStyleWord(true);
+        descriptionPane.add(description);
+        c.gridx = 1;
+        c.ipadx = 150;
+        c.ipady = 80;
+        panel.add(descriptionPane, c);
+
+        add(panel);
+        panel.setBackground(StyleGlobals.BACKGROUND);
+        setBackground(StyleGlobals.BACKGROUND);
     }
 }
