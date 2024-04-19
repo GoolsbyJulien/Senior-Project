4267ff6fef76e440c66ba5b8eaf9b4a813d5aab3
Julien Goolsby <juliengoolsby@gmail.com>
2024-04-19 14:11:50 -0500

    Biome placement redone
    Signed-off-by: Julien Goolsby <juliengoolsby@gmail.com>

--- src/com/jra/app/MapObjects/World.java
+++ src/com/jra/app/MapObjects/World.java
@@ -10,12 +10,15 @@ import com.jra.app.Main;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class World extends MapObject {
 
     private final int WORLD_SIZE = 1600;
     private int mapView = 0;
     float[][] noise = new float[WORLD_SIZE][WORLD_SIZE];
+    float[][] tempV = new float[WORLD_SIZE][WORLD_SIZE];
+
     int[][] biomeMap = new int[WORLD_SIZE][WORLD_SIZE];
     int biomeDensity = 200;
     private PerlinNoise p;
@@ -49,29 +52,64 @@ public class World extends MapObject {
     }
 
 
-    Profiler profiler = new Profiler();
-
     public void generateBiomeMaps(int seed) {
+        Profiler profiler = new Profiler();
 
         profiler.start();
-        for (int i = 0; i < 200; i++) {
 
-            Vector biomePoint = new Vector(Util.RandomRange(0, WORLD_SIZE), Util.RandomRange(0, WORLD_SIZE));
-            int biomeID = Util.RandomRange(1, 2);
-
-            for (int x = -199; x < Util.RandomRange(20, 200); x++) {
-                for (int y = -100; y < 100; y++) {
-                    int xPoint = biomePoint.x + x;
-                    int yPoint = biomePoint.y + y;
+        Random r = new Random(seed);
 
 
-                    if (xPoint < WORLD_SIZE && xPoint > 0 && yPoint < WORLD_SIZE && yPoint > 0)
-                        biomeMap[xPoint][yPoint] = biomeID;
+        PerlinNoise hMap = new PerlinNoise(seed);
+        PerlinNoise tMap = new PerlinNoise(seed);
+        tMap.SetNoiseType(PerlinNoise.NoiseType.OpenSimplex2);
+        tMap.SetFrequency(0.0008f);
+        hMap.SetFrequency(0.007f);
+        tMap.SetFractalType(PerlinNoise.FractalType.FBm);
+        //  tMap.SetDomainWarpAmp(20);
+        tMap.SetFractalOctaves(7);
+        // hMap.SetDomainWarpAmp(20);
+        hMap.SetFractalOctaves(5);
+
+        hMap.SetFractalType(PerlinNoise.FractalType.FBm);
+
+
+        Vector biomePoint = new Vector(r.nextInt(WORLD_SIZE), r.nextInt(WORLD_SIZE));
+        int biomeID = r.nextInt(3);
+        for (int x = 0; x < WORLD_SIZE - WORLD_SIZE / 15; x++) {
+            float c = Util.RandomRange(400, 4550);
+            for (int y = 15; y < WORLD_SIZE - WORLD_SIZE / 15; y++) {
+                double temp = tMap.GetNoise(x, y);
+
+
+                float humidity = hMap.GetNoise(x, y);
+                float e = noise[x][y] * 100;
+
+                //  temp = 10.0 * (e * e) + 100.0 + (10.0 - 10);
+
+//                if (y < WORLD_SIZE / 4 + (-1 * (Math.sin(0.0100 * x * x)) * 10) - 10)
+
+                if (temp < -0.4f && humidity < 0.7)
+                    biomeMap[x][y] = 2;
+
+                else if (temp > 0.3 && humidity > 0.4)
+                    biomeMap[x][y] = 1;
+                else if (humidity < 0.4) {
+                    biomeMap[x][y] = 0;
+                } else {
+                    biomeMap[x][y] = 3;
                 }
+
             }
         }
         profiler.end();
-        System.out.println(profiler.times[0]);
+        System.out.println("Biome Gen Time :" + profiler.times[0] + "ms");
+
+    }
+
+    private boolean pointInMap(int x, int y) {
+
+        return x < WORLD_SIZE && x > 0 && y < WORLD_SIZE && y > 0;
     }
 
     public void refreshNoiseMap() {
@@ -101,7 +139,6 @@ public class World extends MapObject {
     public void generateMap(int seed) {
 
         Profiler worldProfiler = new Profiler();
-        worldProfiler.start();
         bi = new BufferedImage(WORLD_SIZE, WORLD_SIZE, BufferedImage.TYPE_INT_ARGB);
 
         generateBiomeMaps(seed);
@@ -114,9 +151,11 @@ public class World extends MapObject {
         p.SetFractalOctaves(7);
         p.SetNoiseType(PerlinNoise.NoiseType.Perlin);
         Main.instance.currentProject.setPerlinSeed(seed);
+        worldProfiler.start();
+
         refreshNoiseMap();
         worldProfiler.end();
-        System.out.println(worldProfiler.times[0]);
+        System.out.println("World Gen: " + worldProfiler.times[0] + "ms");
 
     }
 
@@ -155,19 +194,36 @@ public class World extends MapObject {
 
 class Biomes {
 
+
     public static Color getDesert(float height) {
 
-        return Color.yellow;
+
+        if (height < 0.4)
+            return Util.RandomRange(0, 1) == 0 ? new Color(225, 180, 73) : new Color(225, 185, 80).darker();
+
+        else if (height < 0.45) return new Color(91, 71, 1);
+        return new Color(213, 116, 8);
     }
 
     public static Color getWinter(float height) {
+        if (height < 0.4) return new Color(253, 253, 253);
 
-        return Color.WHITE;
+        else if (height < 7) return new Color(16, 130, 210);
+
+
+        return Color.white;
     }
 
     public static Color Normal(float height) {
 
-        return Color.red;
+
+        if (height < 0.17)
+            return new Color(225, 180, 73);
+        if (height < 0.4) return new Color(50, 127, 53);
+
+        else if (height < 0.45) return new Color(40, 86, 40);
+
+        return Color.white;
     }
 
 
